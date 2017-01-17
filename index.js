8ffff5c1556dddbe0d4b6be31fffd0fc16bc9adb
Rob Hitt <robhitt@gmail.com>
2017-01-17 15:02:42 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var evens = [0, 2, 4, 6, 8, 10]
+
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+var animals = ["dog", "fish", "cat"]
+
+doToElementsInArray(animals, changeCompletely)
+
+// log out animals -- pretty cool, right?
+console.log(animals)
